903d557c5cc9979761e1b205ac722bbdd4d5533b
Athea DeLing <almdeling@gmail.com>
2022-11-28 21:55:16 -0500

    inital info

---
+++ FriendLetter/Program.cs
@@ -0,0 +1,25 @@
+using Microsoft.AspNetCore.Builder;
+using Microsoft.Extensions.DependencyInjection;
+
+namespace FriendLetter
+{
+  class Program
+  {
+    static void Main(string[] args)
+    {
+      WebApplicationBuilder builder = WebApplication.CreateBuilder(args);
+
+      builder.Services.AddControllerWithViews();
+
+      WebApplication app = builder.Build();
+
+      app.UseRouting();
+
+      app.MapControllerRoute(
+        name: "default",
+        pattern: "{controller=Home}/{action=Index}/{id?}"
+      );
+      app.Run();
+    }
+  }
+}
